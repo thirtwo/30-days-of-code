199cd64d533554374547db27416744b960227912
thirtwo <65511956+thirtwo@users.noreply.github.com>
2021-07-08 23:08:36 +0300

    Day 4: Class vs. Instance

---
+++ Class vs. Instance/Class vs. Instance.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Person {
+    public int age;     
+	public Person(int initialAge) {
+        if(initialAge >= 0){
+        age = initialAge;
+        }
+        else{
+            age = 0;
+            Console.WriteLine("Age is not valid, setting age to 0.");
+        }
+     }
+     public void amIOld() {
+        // Do some computations in here and print out the correct statement to the console 
+        if(age < 13){
+            Console.WriteLine("You are young.");
+        }
+        else if(age >= 13 && age < 18){
+            Console.WriteLine("You are a teenager.");
+        }
+        else{
+            Console.WriteLine("You are old.");
+        }
+     }
+
+     public void yearPasses() {
+       age++;
+     }
+
+static void Main(String[] args) {
+        int T=int.Parse(Console.In.ReadLine());
+        for (int i = 0; i < T; i++) {
+            int age=int.Parse(Console.In.ReadLine());
+            Person p=new Person(age);
+             p.amIOld();
+                for (int j = 0; j < 3; j++) {
+                  p.yearPasses();
+                }
+                p.amIOld();
+                Console.WriteLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 # Day 1: Data Types
 # Day 2: Operators
 # Day 3: Intro to Conditional Statements
+# Day 4: Class vs. Instance
 
 
